31bb34b859fd0ef4c124f540eba0c9dfd5804477
5ec1cff <ewtqyqyewtqyqy@gmail.com>
2023-08-20 05:04:03 +0800

    Support discovery services at runtime

--- app/src/main/java/io/github/a13e300/intenttracker/cli/Main.kt
+++ app/src/main/java/io/github/a13e300/intenttracker/cli/Main.kt
@@ -3,7 +3,6 @@ package io.github.a13e300.intenttracker.cli
 import android.app.IActivityManager
 import android.content.Intent
 import android.os.IBinder
-import android.os.Looper
 import android.os.ServiceManager
 import androidx.core.os.bundleOf
 import io.github.a13e300.intenttracker.broadcastIntentCompat
@@ -14,19 +13,26 @@ import io.github.a13e300.intenttracker.service.IIntentTrackerService
 import io.github.a13e300.intenttracker.service.IServiceFetcher
 import io.github.a13e300.intenttracker.service.IntentTrackerService
 import io.github.a13e300.intenttracker.service.StartActivityInfo
+import java.util.Scanner
 
 class ServiceFetcher : IServiceFetcher.Stub() {
+    private val mServices = mutableSetOf<IBinder>()
     override fun publishService(binder: IBinder) {
         kotlin.runCatching {
             val service = IIntentTrackerService.Stub.asInterface(binder)
             if (service.version != IntentTrackerService.CURRENT_VERSION) {
                 println("remote version not match")
             } else {
-                println("service registered: ${service.serviceInfo}")
+                synchronized(this) {
+                    if (mServices.contains(binder)) return
+                    mServices.add(binder)
+                }
+                val serviceInfo = service.serviceInfo
+                println("service registered: $serviceInfo")
                 service.registerListener(
                     object : IIntentTrackerListener.Stub() {
                         override fun onStartActivity(info: StartActivityInfo) {
-                            println("start activity from ${service.serviceInfo}")
+                            println("start activity from $serviceInfo")
                             info.intent.print()
                             println("stack trace:")
                             info.stackTraceElements.forEach {
@@ -36,7 +42,7 @@ class ServiceFetcher : IServiceFetcher.Stub() {
                         }
 
                         override fun onActivityStarted(info: ActivityStartedInfo) {
-                            println("activity ${info.component} started in ${service.serviceInfo} referrer=${info.referrer}")
+                            println("activity ${info.component} started in $serviceInfo referrer=${info.referrer}")
                             info.intent.print()
                             println("stack trace:")
                             info.stackTraceElements.forEach {
@@ -47,7 +53,14 @@ class ServiceFetcher : IServiceFetcher.Stub() {
                     },
                     IntentTrackerService.FLAG_LISTEN_START_ACTIVITY or IntentTrackerService.FLAG_GET_STACK_TRACE or IntentTrackerService.FLAG_LISTEN_ACTIVITY_STARTED
                 )
-                binder.linkToDeath({ println("${service.serviceInfo} died") }, 0)
+                binder.linkToDeath(
+                    {
+                        println("$serviceInfo died")
+                        synchronized(this) {
+                            mServices.remove(binder)
+                        }
+                    }, 0
+                )
             }
         }.onFailure {
             println("failed to get service")
@@ -56,13 +69,25 @@ class ServiceFetcher : IServiceFetcher.Stub() {
     }
 }
 
-fun main(args: Array<String>) {
-    Looper.prepareMainLooper()
-    val am = IActivityManager.Stub.asInterface(ServiceManager.getService("activity"))
+val am: IActivityManager = IActivityManager.Stub.asInterface(ServiceManager.getService("activity"))
+val fetcher = ServiceFetcher()
+
+fun discoveryServices() {
     am.broadcastIntentCompat(Intent(
         IntentTrackerService.ACTION_REQUIRE_SERVICE
     ).apply {
-        putExtras(bundleOf(IntentTrackerService.KEY_SERVICE_FETCHER to ServiceFetcher()))
+        putExtras(bundleOf(IntentTrackerService.KEY_SERVICE_FETCHER to fetcher))
     })
-    Looper.loop()
+}
+
+fun main(args: Array<String>) {
+    discoveryServices()
+    val scanner = Scanner(System.`in`)
+    while (scanner.hasNextLine()) {
+        val l = scanner.nextLine()
+        when (l) {
+            "r" -> discoveryServices()
+            "q" -> break
+        }
+    }
 }
